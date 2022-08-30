548fef575e94bbd12840809e4786a0470fa30446
Ore Jaiyesimi <orejay2017@gmail.com>
2022-08-30 10:47:56 +0100

    complete with tests and dotenv

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,6 +2,8 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from settings import TEST_DB_NAME, DB_USER, DB_PASSWORD
+
 
 from flaskr import create_app
 from models import setup_db, Question, Category
@@ -14,9 +16,9 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
+        self.database_name = TEST_DB_NAME
         self.database_path = 'postgresql://{}:{}@{}/{}'.format(
-            'postgres', 'password', 'localhost:5432', self.database_name)
+            DB_USER, DB_PASSWORD, 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -32,6 +34,15 @@ class TriviaTestCase(unittest.TestCase):
             "difficulty": 2,
             "id": 5,
             "question": "What country is often referred to as 'the best country in the world'?"
+        }
+
+        self.search_term = {
+            "searchTerm": "who"
+        }
+
+        self.quiz_request_body = {
+            "previous_questions": [],
+            "quiz_category": {"id": "3"}
         }
 
     def tearDown(self):
@@ -63,15 +74,15 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Page not found')
 
-    def test_delete(self):
-        res = self.client().delete('/questions/29')
+    def test_search_question(self):
+        res = self.client().get('/categories/2/questions')
         data = json.loads(res.data)
-
-        question = Question.query.filter(Question.id == 29).one_or_none()
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(question, None)
+        self.assertTrue(data['current_category'])
+        self.assertTrue(data['questions'])
+        self.assertTrue(len(data['questions']))
 
     def test_add_question(self):
         res = self.client().post('/questions', json=self.new_question)
@@ -79,6 +90,33 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+
+    def test_search_question(self):
+        res = self.client().post('/search', json=self.search_term)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(len(data['questions']))
+
+    def test_get_quiz_question(self):
+        res = self.client().post('/quizzes', json=self.quiz_request_body)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+    def test_delete(self):
+        res = self.client().delete('/questions/32')
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == 32).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(question, None)
 
 
 # Make the tests conveniently executable
